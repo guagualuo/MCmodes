9fc0e7d3656733f04fbab88be6779e53da0c3f41
Jiawen <paulljw333@gmail.com>
2022-01-17 19:44:20 +0100

    curl of fields; cylindrical average from spectrum

--- src/fields/spectrum.py
+++ src/fields/spectrum.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 from typing import List, Tuple, Dict
 import numpy as np
 import scipy.sparse as scsp
+from numba import njit
 
 from operators.worland_transform import WorlandTransform
 from operators.associated_legendre_transform import AssociatedLegendreTransformSingleM
@@ -162,7 +163,6 @@ class SpectralComponentSingleM(SpectralComponentBase):
                 'phi': interpolate.interp1d(sg, tmp[:, 1], kind=kind),
                 'z': interpolate.interp1d(sg, tmp[:, 2], kind=kind)}
 
-
     @staticmethod
     def _Wtransform(spectrum, nr, maxnl, m, rg):
         radial = np.full((maxnl-m, rg.shape[0]), fill_value=np.NaN, dtype=np.complex128)
@@ -190,10 +190,35 @@ class SpectralComponentSingleM(SpectralComponentBase):
             radial[l - m, :] = poly @ spectrum[a:b]
         return radial.T
 
+    @staticmethod
+    def _laplacianlWtransform(spectrum, nr, maxnl, m, rg):
+        radial = np.full((maxnl - m, rg.shape[0]), fill_value=np.NaN, dtype=np.complex128)
+        for l in range(m, maxnl):
+            poly = laplacianlW(nr, l, rg)
+            a, b = (l - m) * nr, (l - m + 1) * nr
+            radial[l - m, :] = poly @ spectrum[a:b]
+        return radial.T
+
     def normalise(self, factor):
         self.spectrum /= factor
         if self.energy_spectrum is not None:
             self.energy_spectrum /= np.abs(factor)**2
+
+    def curl(self):
+        """ take curl of the component """
+        if self.component == "tor":
+            self.component = "pol"
+        else:
+            nr, maxnl, m = self.ordering.nr, self.ordering.maxnl, self.ordering.m
+            for l in range(m, maxnl):
+                n_grid = nr + maxnl // 2 + 10
+                rg = worland_grid(n_grid)
+                weight = scsp.diags(np.ones(n_grid) * worland_weight(n_grid))
+                poly = worland(nr, l, rg)
+                lapl_poly = laplacianlW(nr, l, rg)
+                a, b = (l - m) * nr, (l - m + 1) * nr
+                self.spectrum[a:b] = -poly.T @ weight @ lapl_poly.dot(self.spectrum[a:b])
+            self.component = "tor"
 
     def restrict_parity(self, parity):
         """ set the spectrum of certain parity to be zero """
@@ -211,7 +236,7 @@ class SpectralComponentSingleM(SpectralComponentBase):
         nr0, maxnl0, m = self.ordering.nr, self.ordering.maxnl, self.ordering.m
         idx = []
         k = 0
-        for l in range(m, maxnl):
+        for l in range(m, maxnl0):
             for n in range(nr):
                 if n < nr0:
                     idx.append(k)
@@ -228,6 +253,14 @@ class VectorFieldSingleM:
         self.components = {"tor": SpectralComponentSingleM(res, m, "tor", data[:dim]),
                            "pol": SpectralComponentSingleM(res, m, "pol", data[dim:])}
         self.energy_spectrum = 0
+
+    @classmethod
+    def from_components(cls, tor: SpectralComponentSingleM, pol: SpectralComponentSingleM):
+        assert tor.ordering.res == pol.ordering.res and tor.ordering.m == pol.ordering.m
+        res = tor.ordering.res
+        m = tor.ordering.m
+        data = np.concatenate([tor.spectrum, pol.spectrum])
+        return cls(res, m, data)
 
     @classmethod
     def from_parity_spectrum(cls, res, m, data: np.ndarray, parity):
@@ -253,6 +286,14 @@ class VectorFieldSingleM:
         return self.components["tor"].physical_field(worland_transform, legendre_transform) + \
             self.components["pol"].physical_field(worland_transform, legendre_transform)
 
+    def curl(self):
+        """ transform to curl of the field """
+        self.components["tor"].curl()
+        self.components["pol"].curl()
+        new_pol = self.components["tor"]
+        new_tor = self.components["pol"]
+        self.components = {"tor": new_tor, "pol": new_pol}
+
     def normalise(self, factor):
         for comp in self.components.keys():
             component = self.components[comp]
@@ -274,19 +315,95 @@ class VectorFieldSingleM:
     def spectrum(self):
         return np.concatenate([self.components['tor'].spectrum, self.components['pol'].spectrum])
 
+    def cylindrical_average(self, sg, n_jobs=-1, **kwargs) -> Dict[str, Callable]:
+        """ compute cylindrical average of cylindrical components and square of them
+        (can be used to compute columnarity) """
+        # first need to integrate u_s**2 + u_phi**2 on cylinder
+        ordering = self.components['tor'].ordering
+        nr, maxnl, m = ordering.nr, ordering.maxnl, ordering.m
+        zorder = 2*nr + maxnl
+        zorder += zorder % 2 + 8
+        x, w = np.polynomial.legendre.leggauss(zorder)
+
+        def integrate(s):
+            zg = x * np.sqrt(1 - s ** 2)
+            rg = np.sqrt(s ** 2 + zg ** 2)
+            tg = np.arccos(zg / rg)
+            legendre_transform = AssociatedLegendreTransformSingleM(maxnl, m, tg)
+            # toroidal
+            radial = self.components['tor']._Wtransform(self.components['tor'].spectrum, nr, maxnl, m, rg)
+            theta_comp = (1.0j * m * legendre_transform.operators['plmdivsin'] * radial).sum(axis=1)
+            phi_comp = (-legendre_transform.operators['dthetaplm'] * radial).sum(axis=1)
+            # poloidal
+            radial1 = self.components['pol']._divrWtransform(self.components['pol'].spectrum, nr, maxnl, m, rg)
+            radial2 = self.components['tor']._divrdiffrWtransform(self.components['pol'].spectrum, nr, maxnl, m, rg)
+            l_factor = scsp.diags([l * (l + 1) for l in range(m, maxnl)])
+            r_comp = (legendre_transform.operators['plm'] * np.array(radial1 @ l_factor)).sum(axis=1)
+            theta_comp += (legendre_transform.operators['dthetaplm'] * radial2).sum(axis=1)
+            phi_comp += (1.0j * m * legendre_transform.operators['plmdivsin'] * radial2).sum(axis=1)
+
+            s_comp = np.cos(tg) * theta_comp + np.sin(tg) * r_comp
+            z_comp = -np.sin(tg) * theta_comp + np.cos(tg) * r_comp
+            s_comp2 = np.abs(s_comp)**2
+            phi_comp2 = np.abs(phi_comp)**2
+            z_comp2 = np.abs(z_comp)**2
+            return [0.5 * (s_comp * w).sum(), 0.5 * (phi_comp * w).sum(), 0.5 * (z_comp * w).sum(),
+                    0.5 * (s_comp2 * w).sum(), 0.5 * (phi_comp2 * w).sum(), 0.5 * (z_comp2 * w).sum()]
+
+        from joblib import Parallel, delayed
+        from scipy.interpolate import interpolate
+        tmp = np.array(Parallel(n_jobs=n_jobs,
+                                verbose=kwargs.get('verbose', 0),
+                                batch_size=kwargs.get('batch_size', 1))(delayed(integrate)(s) for s in sg))
+        kind = kwargs.get('interp_kind', 'cubic')
+        return {'s': interpolate.interp1d(sg, tmp[:, 0], kind=kind),
+                'phi': interpolate.interp1d(sg, tmp[:, 1], kind=kind),
+                'z': interpolate.interp1d(sg, tmp[:, 2], kind=kind),
+                's_square': interpolate.interp1d(sg, tmp[:, 3], kind=kind),
+                'phi_square': interpolate.interp1d(sg, tmp[:, 4], kind=kind),
+                'z_square': interpolate.interp1d(sg, tmp[:, 5], kind=kind)
+                }
+
 
 if __name__ == "__main__":
-    sp = SpectralComponentSingleM.from_modes((101, 201), 1, 'tor', [(2, 2, 1), (3, 2, 1), (4, 2, 1)])
+    # sp = SpectralComponentSingleM.from_modes((101, 201), 1, 'tor', [(2, 2, 1), (3, 2, 1), (4, 2, 1)])
+    # sg = np.linspace(0.0, 1, 101)
+    # with Timer("compute cylindrical integration"):
+    #     u_av = sp.cylindrical_integration(sg, n_jobs=1)
+    #
+    # def reference(s):
+    #     from math import sqrt, pi
+    #     return (1 / (1155 * sqrt(21) * pi)) * (2409 * sqrt(2) + 1264 * sqrt(55) - \
+    #                                            4 * (17457 * sqrt(2) + 11749 * sqrt(55)) * s ** 2 + 24 * (9416 * sqrt(2) + 10735 * sqrt(55)) * s ** 4 - 256 * (660 * sqrt(2) + 1723 * sqrt(55)) * s ** 6 + 232960 * sqrt(55) * s ** 8)
+    #
+    # plt.plot(sg, u_av['phi'](sg).real)
+    # plt.plot(sg, u_av['phi'](sg).imag)
+    # plt.plot(sg, reference(sg)-u_av['phi'](sg).real)
+    # plt.show()
+
+    tor_sp = SpectralComponentSingleM.from_modes((21, 41), 1, 'tor', [(1, 1, 1), (2, 1, 1)])
+    pol_sp = SpectralComponentSingleM.from_modes((21, 41), 1, 'pol', [(1, 1, 1), (2, 1, 1)])
+    sp = VectorFieldSingleM.from_components(tor_sp, pol_sp)
     sg = np.linspace(0.0, 1, 101)
-    with Timer("compute cylindrical integration"):
-        u_av = sp.cylindrical_integration(sg, n_jobs=8)
+    av = sp.cylindrical_average(sg, n_jobs=1)
 
-    def reference(s):
-        from math import sqrt, pi
-        return (1 / (1155 * sqrt(21) * pi)) * (2409 * sqrt(2) + 1264 * sqrt(55) - \
-                                               4 * (17457 * sqrt(2) + 11749 * sqrt(55)) * s ** 2 + 24 * (9416 * sqrt(2) + 10735 * sqrt(55)) * s ** 4 - 256 * (660 * sqrt(2) + 1723 * sqrt(55)) * s ** 6 + 232960 * sqrt(55) * s ** 8)
+    def reference(s, c):
+        from math import sqrt, pi, exp
+        if c == 's':
+            return (sqrt(2)*(14j + 5*sqrt(3) - 2*(19j + 10*sqrt(3))*s**2 + 24j*s**4))/(15*pi)
+        elif c == 'phi':
+            return (sqrt(2)*(-14 + 5j*sqrt(3) + 16*(8 - 5j*sqrt(3))*s**2 - 144*s**4))/(15*pi)
+        elif c == 'z':
+            return s*(156 - 5j*sqrt(3) + 8j*(42j + sqrt(3))*s**2)/(3*sqrt(2)*pi)
+        elif c == 's_square':
+            return (15927 - 56147*s**2 + 80320*s**4 - 40192*s**6 + 512*s**8)/(70*pi**2)
+        elif c == 'phi_square':
+            return (47781 - 96121*s**2 + 232432*s**4 - 193920*s**6 + 43008*s**8)/(210*pi**2)
+        elif c == 'z_square':
+            return (299819*s**2 - 1253216*s**4 + 1336320*s**6 - 4608*s**8)/(210*pi**2)
 
-    plt.plot(sg, u_av['phi'](sg).real)
-    plt.plot(sg, u_av['phi'](sg).imag)
-    plt.plot(sg, reference(sg)-u_av['phi'](sg).real)
-    plt.show()
+    for k in av.keys():
+        print(np.max(np.abs(av[k](sg) - reference(sg, k))))
+        
+    
+
